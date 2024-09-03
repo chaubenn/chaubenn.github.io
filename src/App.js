8c5d4a1208c4b775aa6761cfc2dc79f1bddd5e89
Ben Chau <benjaminchau05@gmail.com>
2024-09-03 19:47:56 +1000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome to My GitHub Page</h1>
+                <p>
+                    This is a simple React app deployed on GitHub Pages.
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
